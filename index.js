14e36c6edcfd9809e9c13297b7a7e22373abdb98
Clemente Estrada <mirdrack@gmail.com>
2016-04-04 14:52:11 -0500

    Added a route tracker

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var http = require('http');
+var url = require('url');
 
-http.createServer(function(request, response) {
-  
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write("Hola Mundo");
-  response.end();
 
-}).listen(8000);
+http.createServer(onRequest).listen(8000);
+
+function onRequest(request, response) {
+
+	console.log('Server ready...');
+
+	var pathname = url.parse(request.url).pathname;
+    console.log('Request for:' + pathname);
+
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.write('Hello Im a random route');
+	response.end();
+
+}
